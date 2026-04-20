a93e198e4dbbc8908027df7b08a46d0f37ba977b
sonmai123 <mainganson11146@gmail.com>
2026-04-20 19:13:26 +0700

    Fix Express wildcard OPTIONS route for Railway/Express 5

--- server/index.js
+++ server/index.js
@@ -68,7 +68,7 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.options("*", cors(corsOptions));
+app.options("/*", cors(corsOptions));
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", req.headers.origin || "*");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, Range");
